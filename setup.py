402b71aba5fe8b673173a1056a352ef7bff2ed93
MBanucu <michael.banucu@googlemail.com>
2026-06-10 15:39:32 +0200

    chore: drop EOL Python versions, require >=3.10

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # version=,
     license="GPLv3+/BSD",
     url="https://github.com/MBanucu/pyexiftool",
-    python_requires=">=3.6",
+    python_requires=">=3.10",
 
     # authors
     author="MBanucu",
@@ -90,13 +90,10 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
 
         "Topic :: Multimedia",
         "Topic :: Utilities",
